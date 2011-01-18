c5ca956f00486995705a1a97778214fec2b6a86c
Chris <cjw7488@gmail.com>
2011-01-18 14:28:44 -0600

    User model updated with passwords
    exit

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Chris Walker"
+  user.email                  "cjw7488@gmail.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
