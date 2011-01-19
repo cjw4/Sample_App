318fcb0d4b1fd49917d780acf723bc8263390622
Chris <cjw7488@gmail.com>
2011-01-19 15:02:05 -0600

    implemented the user sign up part of the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@
 
 class User < ActiveRecord::Base
   attr_accessor :password
-  attr_accessible :name, :email, :password, :confirm_password
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
